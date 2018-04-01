ab76b32dafa8ca0813588619981e687a5bee280b
Saurabh Dutta <saurabhdutta.jk@gmail.com>
2018-04-01 21:56:10 +0530

    Change Notification

--- src/app/components/notification/notification.component.ts
+++ src/app/components/notification/notification.component.ts
@@ -13,8 +13,8 @@ export class NotificationComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.notices.push('<span>Registration for new batches start from 10th April 2018.</span>');
-    this.notices.push('<span>Mega Talent Scholarship exam on 28th April 2018.</span>');
+    this.notices.push('<span>Talent Hunt Exam on 28th April 2018.</span>');
+    this.notices.push('<span>Registration starts for Talent Hunt Exam starts from 11th April 2018</span>');
     this.notices.push('<span>Discount up-to 30% for scholarship winners. Special additional discount for all exam participants.</span>');
     this.notices.push('<span>New batches are starting from 15th May 2018.</span>');
     this.notices.push('<span>Faculty recruitment process is ongoing now (Freshers can also apply).</span>');
