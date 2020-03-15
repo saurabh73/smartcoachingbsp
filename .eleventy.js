017169914911c83235f5be51a2ba323d72d35ad4
Saurabh Dutta <saurabhdutta.jk@gmail.com>
2020-03-15 22:24:03 +0000

    add SEO field

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,11 @@
+const pluginSEO = require("eleventy-plugin-seo");
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("favicon.ico");
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy('src/images')
+
+  eleventyConfig.addPlugin(pluginSEO, require("./src/_data/seo.json"));
 
   return {
     dir: { input: 'src', output: 'dist', data: '_data' },
