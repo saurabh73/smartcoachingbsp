90e95c2d79156e730cf5ceef080bb19946daac14
Saurabh Dutta <saurabhdutta.jk@gmail.com>
2020-03-16 23:15:46 +0000

    update content

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,31 @@
 const pluginSEO = require("eleventy-plugin-seo");
+const markdownIt = require("markdown-it");
 const yaml = require("js-yaml");
 
+// const yaml = require("js-yaml");
+
 module.exports = function(eleventyConfig) {
+  
+  // Enable Markdown Render
+  const md = new markdownIt({
+    html: true,
+    xhtmlOut: true,
+    breaks: true
+  });
+  
+
   eleventyConfig.addPassthroughCopy("favicon.ico");
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy('src/images')
 
   eleventyConfig.addPlugin(pluginSEO, require("./src/_data/seo.json"));
+ 
+  eleventyConfig.addPairedShortcode("markdown", (content) => {
+    return md.renderInline(content);
+  });
   eleventyConfig.addDataExtension("yaml", contents => yaml.safeLoad(contents));
+
+  // eleventyConfig.addDataExtension("yaml", contents => yaml.safeLoad(contents));
 
   return {
     dir: { input: 'src', output: 'dist', data: '_data' },
