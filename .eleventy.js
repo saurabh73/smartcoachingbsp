1b6ea23273433b44ea1e8770b901884e5cec74b3
Saurabh Dutta <saurabhdutta.jk@gmail.com>
2020-03-17 00:06:58 +0000

    update stucture

--- .eleventy.js
+++ .eleventy.js
@@ -2,37 +2,36 @@ const pluginSEO = require("eleventy-plugin-seo");
 const markdownIt = require("markdown-it");
 const yaml = require("js-yaml");
 
-// const yaml = require("js-yaml");eleventyConfig.addDataExtension("yaml", contents => yaml.safeLoad(contents));
+module.exports = function (eleventyConfig) {
 
-module.exports = function(eleventyConfig) {
-  
   // Enable Markdown Render
   const md = new markdownIt({
     html: true,
     xhtmlOut: true,
-    breaks: true
+    breaks: false
   });
-  
 
-  eleventyConfig.addPassthroughCopy("favicon.ico");
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
   eleventyConfig.addPassthroughCopy("admin");
-  eleventyConfig.addPassthroughCopy('src/images')
+  eleventyConfig.addPassthroughCopy("src/images");
 
-  eleventyConfig.addDataExtension("yaml", contents => yaml.safeLoad(contents));
+  eleventyConfig.addDataExtension("yaml", (contents) => {
+    return yaml.safeLoad(contents)
+  });
 
   eleventyConfig.addPlugin(pluginSEO, require("./src/_data/seo.json"));
- 
+
   eleventyConfig.addPairedShortcode("markdown", (content) => {
     return md.renderInline(content);
   });
-  
 
-  // eleventyConfig.addDataExtension("yaml", contents => yaml.safeLoad(contents));
+
+
 
   return {
-    dir: { input: 'src', output: 'dist', data: '_data' },
+    dir: { input: "src", output: "dist", data: "_data" },
     passthroughFileCopy: true,
-    templateFormats: ['njk', 'md', 'css', 'html', 'yml'],
-    htmlTemplateEngine: 'njk'
+    templateFormats: ["njk", "md", "css", "html", "yml"],
+    htmlTemplateEngine: "njk"
   }
 }
