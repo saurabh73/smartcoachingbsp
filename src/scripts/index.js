f608b9a875f291a9f3824c2de71949378cccef91
Saurabh Dutta <saurabhdutta.jk@gmail.com>
2020-03-20 02:04:13 +0000

    Final Changes

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -85,4 +85,113 @@ import "slick-carousel";
     }
   }
   mainSlider();
-})(jQuery);
+  /* 4. Testimonial Active*/
+  var testimonial = $('.h1-testimonial-active');
+  if (testimonial.length) {
+    testimonial.slick({
+      dots: false,
+      infinite: true,
+      speed: 1000,
+      autoplay: true,
+      loop: true,
+      arrows: false,
+      prevArrow: '<button type="button" class="slick-prev"><i class="ti-angle-left"></i></button>',
+      nextArrow: '<button type="button" class="slick-next"><i class="ti-angle-right"></i></button>',
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false,
+            arrow: false
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+          }
+        }
+      ]
+    });
+  }
+
+  /* 4. Notices Active*/
+  var notice = $('.h1-notice-active');
+  if (notice.length) {
+    notice.slick({
+      dots: false,
+      infinite: true,
+      speed: 1000,
+      autoplay: true,
+      loop: true,
+      arrows: false,
+      prevArrow: '<button type="button" class="slick-prev"><i class="ti-angle-left"></i></button>',
+      nextArrow: '<button type="button" class="slick-next"><i class="ti-angle-right"></i></button>',
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false,
+            arrow: false
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+          }
+        }
+      ]
+    });
+  }
+
+/* 5.  Custom Sticky Menu  */
+  $(window).on('scroll', function () {
+      var scroll = $(window).scrollTop();
+      if (scroll < 245) {
+        $(".header-sticky").removeClass("sticky-bar");
+      } else {
+        $(".header-sticky").addClass("sticky-bar");
+      }
+    });
+
+    $(window).on('scroll', function () {
+      var scroll = $(window).scrollTop();
+      if (scroll < 245) {
+        $(".header-sticky").removeClass("sticky");
+      } else {
+        $(".header-sticky").addClass("sticky");
+      }
+    });
+
+  }) (jQuery);
